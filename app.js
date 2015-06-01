85388fd0fcc5f7a62e19322a9c8bda5a772db904
Raphael Ochsenbein <raphael@akehir.com>
2015-06-01 08:34:17 +0200

    Reformatted code.

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var moment = require('moment');
 var async = require('async');
 
 // Settings
-var dbKeywordsCollection	= "keywords";
-var dbResultsCollection		= "results";
-var dbCacheCollection		= "cache";
+var dbKeywordsCollection = "keywords";
+var dbResultsCollection = "results";
+var dbCacheCollection = "cache";
 
 var mqlightTweetsTopic = "mqlight/ase/tweets";
 var mqlightAnalyzedTopic = "mqlight/ase/analyzed";
@@ -26,32 +26,31 @@ var mqlightClient = null;
 var opts = {};
 var mqlightService = {};
 if (process.env.VCAP_SERVICES) {
-  var services = JSON.parse(process.env.VCAP_SERVICES);
-  console.log( 'Running BlueMix');
-  if (services[ mqlightServiceName ] == null) {
-    throw 'Error - Check that app is bound to service';
-  }
-  mqlightService = services[mqlightServiceName][0];
-  opts.service = mqlightService.credentials.connectionLookupURI;
-  opts.user = mqlightService.credentials.username;
-  opts.password = mqlightService.credentials.password;
+    var services = JSON.parse(process.env.VCAP_SERVICES);
+    console.log('Running BlueMix');
+    if (services[mqlightServiceName] == null) {
+        throw 'Error - Check that app is bound to service';
+    }
+    mqlightService = services[mqlightServiceName][0];
+    opts.service = mqlightService.credentials.connectionLookupURI;
+    opts.user = mqlightService.credentials.username;
+    opts.password = mqlightService.credentials.password;
 } else {
-  opts.service = 'amqp://localhost:5672';
+    opts.service = 'amqp://localhost:5672';
 }
 
 
 // defensiveness against errors parsing request bodies...
 process.on('uncaughtException', function (err) {
-	console.log('Caught exception: ' + err.stack);
+    console.log('Caught exception: ' + err.stack);
 });
 
 var app = express();
 // Configure the app web container
-app.configure(function() {
-	app.use(express.bodyParser());
-	app.use(express.static(__dirname + '/public'));
+app.configure(function () {
+    app.use(express.bodyParser());
+    app.use(express.static(__dirname + '/public'));
 });
-
 
 
 // Database Connection
@@ -69,146 +68,146 @@ if (process.env.VCAP_SERVICES) {
 
     console.log("Mongo URL:" + mongo.url);
 } else {
-   console.log("No VCAP Services!");
-   mongo['url'] = "mongodb://localhost:27017/ase";
-} 
-
-var myDb; 
-var mongoConnection = mongoClient.connect(mongo.url, function(err, db) {
-    
-   if(!err) {
-    console.log("Connection to mongoDB established");
-    myDb = db;
-
-	keywordsCollection = myDb.collection(dbKeywordsCollection);
-	resultsCollection = myDb.collection(dbResultsCollection);
-	cacheCollection = myDb.collection(dbCacheCollection);
-
-	// Start the App after DB Connection
-	startApp();
-
-  } else {
-  	console.log("Failed to connect to database!");
-  }
-}); 
-
-
-function startApp() {
-	/*
-	 * Create our MQ Light client
-	 * If we are not running in Bluemix, then default to a local MQ Light connection  
-	 */
-	 
-	runGC();
-	 
-	mqlightClient = mqlight.createClient(opts, function(err) {
-	    if (err) {
-	      console.error('Connection to ' + opts.service + ' using client-id ' + mqlightClient.id + ' failed: ' + err);
-	    }
-	    else {
-	      console.log('Connected to ' + opts.service + ' using client-id ' + mqlightClient.id);
-	    }
-	    /*
-	     * Create our subscription
-	     */
-	    mqlightClient.on('message', processMessage);
-	    mqlightClient.subscribe(mqlightAggregateTopic, mqlightShareID, 
-	        {credit : 5,
-	           autoConfirm : true,
-	           qos : 0}, function(err) {
-	             if (err) console.error("Failed to subscribe: " + err); 
-	             else {
-	               console.log("Subscribed");
-	               mqlightSubInitialised = true;
-	             }
-	           });
-	  });
+    console.log("No VCAP Services!");
+    mongo['url'] = "mongodb://localhost:27017/ase";
 }
 
+var myDb;
+var mongoConnection = mongoClient.connect(mongo.url, function (err, db) {
+
+    if (!err) {
+        console.log("Connection to mongoDB established");
+        myDb = db;
+
+        keywordsCollection = myDb.collection(dbKeywordsCollection);
+        resultsCollection = myDb.collection(dbResultsCollection);
+        cacheCollection = myDb.collection(dbCacheCollection);
+
+        // Start the App after DB Connection
+        startApp();
+
+    } else {
+        console.log("Failed to connect to database!");
+    }
+});
+
+function startApp() {
+    /*
+     * Create our MQ Light client
+     * If we are not running in Bluemix, then default to a local MQ Light connection
+     */
+
+    runGC();
+
+    mqlightClient = mqlight.createClient(opts, function (err) {
+        if (err) {
+            console.error('Connection to ' + opts.service + ' using client-id ' + mqlightClient.id + ' failed: ' + err);
+        }
+        else {
+            console.log('Connected to ' + opts.service + ' using client-id ' + mqlightClient.id);
+        }
+        /*
+         * Create our subscription
+         */
+        mqlightClient.on('message', processMessage);
+        mqlightClient.subscribe(mqlightAggregateTopic, mqlightShareID,
+            {
+                credit: 5,
+                autoConfirm: true,
+                qos: 0
+            }, function (err) {
+                if (err) console.error("Failed to subscribe: " + err);
+                else {
+                    console.log("Subscribed");
+                    mqlightSubInitialised = true;
+                }
+            });
+    });
+}
 
 /*
  * Handle each message as it arrives
  */
 function processMessage(data, delivery) {
-	  var analyzed = data.analyzed;
-	  try {
-	    // Convert JSON into an Object we can work with 
-	    data = JSON.parse(data);
-	    analyzed = data.analyzed;
-	  } catch (e) {
-	    // Expected if we already have a Javascript object
-	  }
-	  if (!analyzed) {
-	    console.error("Bad data received: " + data);
-	  }
-	  else {
-	    //console.log("Received data: " + JSON.stringify(data));
-	    // Upper case it and publish a notification
-	    
-	    updateCache(analyzed.phrase, analyzed.date);
-	  }
+    var analyzed = data.analyzed;
+    try {
+        // Convert JSON into an Object we can work with
+        data = JSON.parse(data);
+        analyzed = data.analyzed;
+    } catch (e) {
+        // Expected if we already have a Javascript object
+    }
+    if (!analyzed) {
+        console.error("Bad data received: " + data);
+    }
+    else {
+        //console.log("Received data: " + JSON.stringify(data));
+        // Upper case it and publish a notification
+
+        updateCache(analyzed.phrase, analyzed.date);
+    }
 }
 
 
 function runGC() {
- setInterval(function () {    //  call a 30s setTimeout when the loop is called
-		console.log("Running GC.");
-		global.gc();
-		console.log("Completed GC.");
-	}, 30000)
+    setInterval(function () {    //  call a 30s setTimeout when the loop is called
+        console.log("Running GC.");
+        global.gc();
+        console.log("Completed GC.");
+    }, 30000)
 }
 
 function updateCache(phrase, date) {
 
-	var dayMoment = moment(date).startOf('day');
-	var startDate = dayMoment.toISOString();
-	var endDate   = dayMoment.endOf('day').toISOString();
+    var dayMoment = moment(date).startOf('day');
+    var startDate = dayMoment.toISOString();
+    var endDate = dayMoment.endOf('day').toISOString();
 
-	var findObject = {
-		phrase: phrase,
-		date: {
-	        $gte: startDate,
-	        $lt: endDate
-    	}
-	};
+    var findObject = {
+        phrase: phrase,
+        date: {
+            $gte: startDate,
+            $lt: endDate
+        }
+    };
 
 
-	resultsCollection.aggregate([
-			{
-				$match: findObject
-			},
-			{
-				$group: {
-					_id: "$phrase",
-					totalsentiment: { $sum: "$sentiment" },
-					tweets: { $sum: 1 }
-				}
-			}
-		],
-		function(err, result) {
+    resultsCollection.aggregate([
+            {
+                $match: findObject
+            },
+            {
+                $group: {
+                    _id: "$phrase",
+                    totalsentiment: {$sum: "$sentiment"},
+                    tweets: {$sum: 1}
+                }
+            }
+        ],
+        function (err, result) {
 
-			console.log(result);
-			var tweets = result.tweets
+            console.log(result);
+            var tweets = result.tweets
 
-			resultsCollection.find(findObject).sort({date: -1}).limit(5).toArray(function(err, docs) {
-				var latestTweets = [];
+            resultsCollection.find(findObject).sort({date: -1}).limit(5).toArray(function (err, docs) {
+                var latestTweets = [];
 
-				docs.forEach(function(tweet, index) {
-					latestTweets.push(tweet);
-				});
+                docs.forEach(function (tweet, index) {
+                    latestTweets.push(tweet);
+                });
 
-				var cacheEntry = {
-							phrase: phrase,
-							date: startDate,
-							tweets: result[0].tweets,
-							totalsentiment: result[0].totalsentiment,
-							latestTweets: latestTweets
-						};
+                var cacheEntry = {
+                    phrase: phrase,
+                    date: startDate,
+                    tweets: result[0].tweets,
+                    totalsentiment: result[0].totalsentiment,
+                    latestTweets: latestTweets
+                };
 
-				cacheCollection.update({phrase: phrase, date: startDate}, cacheEntry, {upsert: true});
-			});
+                cacheCollection.update({phrase: phrase, date: startDate}, cacheEntry, {upsert: true});
+            });
 
-		});
+        });
 }
 
 
